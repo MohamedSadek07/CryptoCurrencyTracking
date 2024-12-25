f9022f5f09632c9fc50ae34282b17b67d685f3d1
Mohamed Sadek <msadek.c@channels.com.sa>
2024-12-25 19:47:37 +0200

    Initial Commit

---
+++ CryptoCurrencyTracking/CryptoCurrencyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyTrackingApp.swift
+//  CryptoCurrencyTracking
+//
+//  Created by Mohamed Sadek on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
